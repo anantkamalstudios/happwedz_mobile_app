8c5b62916c6b87c82eb9235f737130cda9883ccc
Harshada <harshada.anantkamalstudios@gmail.com>
2026-09-21 11:43:23 +0530

    version update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.0.20"
+        versionCode = 21
+        versionName = "1.0.21"
     }
 //    signingConfigs {
 //        release {
